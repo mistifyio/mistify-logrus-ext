1daf9731527261568e789fe10e03fc095d095dc1
Brian Bickerton <brian.bickerton@gmail.com>
2015-09-09 04:57:42 +1000

    Switch to testify and improve test code coverage

--- ext_test.go
+++ ext_test.go
@@ -3,65 +3,98 @@ package logrusx_test
 import (
 	"bytes"
 	"errors"
-	"strings"
+	"io"
 	"testing"
 
 	log "github.com/Sirupsen/logrus"
-	h "github.com/bakins/test-helpers"
 	logx "github.com/mistifyio/mistify-logrus-ext"
+	"github.com/stretchr/testify/suite"
 )
 
-func TestSetLevel(t *testing.T) {
-	var err error
-	defer log.SetLevel(log.InfoLevel)
-	err = logx.SetLevel("foobar")
-	h.Assert(t, err != nil, "expected log level error")
-
-	err = logx.SetLevel("debug")
-	h.Ok(t, err)
-
-	std := log.StandardLogger()
-	h.Equals(t, log.DebugLevel, std.Level)
-
+type ExtTestSuite struct {
+	suite.Suite
+	DefaultFormatter log.Formatter
+	DefaultLevel     log.Level
+	DefaultOut       io.Writer
 }
 
-func TestDefaultSetup(t *testing.T) {
-	var err error
-	defer log.SetLevel(log.InfoLevel)
-	defer log.SetFormatter(&log.TextFormatter{})
-	err = logx.DefaultSetup("foobar")
-	h.Assert(t, err != nil, "expected log level error")
-
-	err = logx.DefaultSetup("debug")
-	h.Ok(t, err)
-
-	std := log.StandardLogger()
-	_, ok := std.Formatter.(*logx.MistifyFormatter)
-	h.Assert(t, ok, "expected MistifyFormatter to be set")
+func (s *ExtTestSuite) restoreDefaults() {
+	// Restore logrus defaults
+	log.SetFormatter(s.DefaultFormatter)
+	log.SetLevel(s.DefaultLevel)
+	log.SetOutput(s.DefaultOut)
 }
 
-func TestLogReturnedErr(t *testing.T) {
+func (s *ExtTestSuite) SetupSuite() {
+	// Save logrus defaults
+	std := log.StandardLogger()
+	s.DefaultFormatter = std.Formatter
+	s.DefaultLevel = std.Level
+	s.DefaultOut = std.Out
+}
+
+func (s *ExtTestSuite) SetupTest() {
+	// Make sure each test starts with defaults
+	s.restoreDefaults()
+}
+
+func (s *ExtTestSuite) TearDownSuite() {
+	// Make sure defaults are restored for other test suites
+	s.restoreDefaults()
+}
+
+func TestExtTestSuite(t *testing.T) {
+	suite.Run(t, new(ExtTestSuite))
+}
+
+func (s *ExtTestSuite) TestSetLevel() {
+	// Bad value
+	s.Error(logx.SetLevel("foobar"))
+	s.Equal(s.DefaultLevel, log.GetLevel())
+
+	// Good value
+	s.NoError(logx.SetLevel("debug"))
+	s.Equal(log.DebugLevel, log.GetLevel())
+}
+
+func (s *ExtTestSuite) TestDefaultSetup() {
+	// Bad Value
+	s.Error(logx.DefaultSetup("foobar"))
+	s.Equal(s.DefaultLevel, log.GetLevel())
+
+	// Good Value
+	std := log.StandardLogger()
+	s.NoError(logx.DefaultSetup("debug"))
+	s.Equal(log.DebugLevel, log.GetLevel())
+	s.IsType(&logx.MistifyFormatter{}, std.Formatter)
+}
+
+func (s *ExtTestSuite) TestLogReturnedErr() {
 	var buffer bytes.Buffer
 	var out string
-
 	log.SetOutput(&buffer)
 
-	fn := func() error {
+	logMsg := "qwerty"
+	errMsg := "foobar"
+
+	// No error logged
+	returnsNil := func() error {
 		return nil
 	}
 
-	logx.LogReturnedErr(fn, nil, "qwerty")
+	logx.LogReturnedErr(returnsNil, nil, logMsg)
 	out = buffer.String()
-	h.Assert(t, !strings.Contains(out, "foobar"), "did not expect error logged")
-	h.Assert(t, !strings.Contains(out, "qwerty"), "did not expect message logged")
+	s.NotContains(out, logMsg)
+	s.NotContains(out, errMsg)
 	buffer.Truncate(0)
 
-	fnE := func() error {
-		return errors.New("foobar")
+	// Error logged
+	returnsError := func() error {
+		return errors.New(errMsg)
 	}
 
-	logx.LogReturnedErr(fnE, nil, "qwerty")
+	logx.LogReturnedErr(returnsError, nil, logMsg)
 	out = buffer.String()
-	h.Assert(t, strings.Contains(out, "foobar"), "expected error logged")
-	h.Assert(t, strings.Contains(out, "qwerty"), "expected message logged")
+	s.Contains(out, logMsg)
+	s.Contains(out, errMsg)
 }
